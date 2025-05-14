569e01bb705b03d8986354f2e3dbf9808a09bb7d
webreflection <andrea.giammarchi@gmail.com>
2025-05-14 09:49:39 +0200

    simplified heap definition

--- src/heap.js
+++ src/heap.js
@@ -12,22 +12,13 @@
  * @param {number} [id=0] The initial `id` which is `0` by default.
  * @param {Map<number, any>} [ids=new Map] The used map of ids to references.
  * @param {Map<any, number>} [refs=new Map] The used map of references to ids.
- * @returns
+ * @returns {Heap<any>}
  */
 export default (id = 0, ids = new Map, refs = new Map) => ({
-  /**
-   * Clear the heap.
-   */
   clear: () => {
     ids.clear();
     refs.clear();
   },
-
-  /**
-   * Get the id of a reference.
-   * @param {any} ref
-   * @returns {number}
-   */
   id: ref => {
     let uid = refs.get(ref);
     if (uid === void 0) {
@@ -38,19 +29,7 @@ export default (id = 0, ids = new Map, refs = new Map) => ({
     }
     return uid;
   },
-
-  /**
-   * Get the reference of an id.
-   * @param {number} id
-   * @returns {any}
-   */
   ref: id => ids.get(id),
-
-  /**
-   * Unref an id.
-   * @param {number} id
-   * @returns {boolean}
-   */
   unref: id => {
     refs.delete(ids.get(id));
     return ids.delete(id);
--- types/heap.d.ts
+++ types/heap.d.ts
@@ -1,27 +1,4 @@
-declare function _default(id?: number, ids?: Map<number, any>, refs?: Map<any, number>): {
-    /**
-     * Clear the heap.
-     */
-    clear: () => void;
-    /**
-     * Get the id of a reference.
-     * @param {any} ref
-     * @returns {number}
-     */
-    id: (ref: any) => number;
-    /**
-     * Get the reference of an id.
-     * @param {number} id
-     * @returns {any}
-     */
-    ref: (id: number) => any;
-    /**
-     * Unref an id.
-     * @param {number} id
-     * @returns {boolean}
-     */
-    unref: (id: number) => boolean;
-};
+declare function _default(id?: number, ids?: Map<number, any>, refs?: Map<any, number>): Heap<any>;
 export default _default;
 export type Heap<T> = {
     clear: () => void;
