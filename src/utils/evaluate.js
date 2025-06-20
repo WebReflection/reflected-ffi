801ad96c5248dee4b8eb334267ff1e35526e5268
webreflection <andrea.giammarchi@gmail.com>
2025-06-20 10:18:27 +0200

    cleanup of unused files

--- src/utils/evaluate.js
+++
@@ -1,8 +0,0 @@
-/**
- * Invokes the given callback with the given arguments.
- * This utility is here only to simplify code portability.
- * @param {Function} callback
- * @param  {...any} args
- * @returns {any}
- */
-export default (callback, ...args) => callback.apply(null, args);
--- types/utils/sab-decoder.d.ts
+++
@@ -1,2 +0,0 @@
-export default canDecode;
-declare let canDecode: boolean;
