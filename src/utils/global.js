f355d755654f8e40ef9786a7482e2013a4346b11
webreflection <andrea.giammarchi@gmail.com>
2025-07-10 11:21:18 -0400

    Workaround for https://github.com/micropython/micropython/issues/17657

--- src/utils/global.js
+++ src/utils/global.js
@@ -5,7 +5,9 @@ const { construct } = Reflect;
 const { toStringTag } = Symbol;
 const { toString } = object;
 
-export const toName = (ref, name = toString.call(ref).slice(8, -1)) =>
+// TODO: just use `ref` once this bug has been fixed:
+//       https://github.com/micropython/micropython/issues/17657
+export const toName = (ref, name = toString.call(getPrototypeOf(ref)).slice(8, -1)) =>
   name in globalThis ? name : toName(getPrototypeOf(ref) || object);
 
 export const toTag = (ref, name = ref[toStringTag]) =>
