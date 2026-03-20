4909bfb9d541881f63b17fa809de064f99de40b4
webreflection <andrea.giammarchi@gmail.com>
2026-03-20 10:46:55 +0100

    publishing on pip

--- pypi.sh
+++ pypi.sh
@@ -29,49 +29,55 @@ setup(
 
 npm run build:py
 
+if [ "$1" = "publish" ]; then
+    python -m twine upload --verbose --repository reflected_ffi pypi/*
+else
+    cat setup.py 
+fi
+
 rm -f setup.py
 
-echo '<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <title>reflected_ffi</title>
-        <link rel="stylesheet" href="https://pyscript.net/releases/2025.11.2/core.css">
-        <script type="module">
-          import { encode, decode } from "https://esm.run/reflected-ffi/direct";
+# echo '<!DOCTYPE html>
+# <html lang="en">
+#     <head>
+#         <meta charset="utf-8">
+#         <meta name="viewport" content="width=device-width,initial-scale=1">
+#         <title>reflected_ffi</title>
+#         <link rel="stylesheet" href="https://pyscript.net/releases/2025.11.2/core.css">
+#         <script type="module">
+#           import { encode, decode } from "https://esm.run/reflected-ffi/direct";
 
-          globalThis.encode = encode;
+#           globalThis.encode = encode;
 
-          // MicroPython does not integrate bytes or bytearray properly
-          // but it is possible to iterate via their proxy wrapper
-          globalThis.decode = value => decode("_ref" in value ? value : [...value]);
+#           // MicroPython does not integrate bytes or bytearray properly
+#           // but it is possible to iterate via their proxy wrapper
+#           globalThis.decode = value => decode("_ref" in value ? value : [...value]);
 
-          // be sure PyScript core is imported after global utilities are defined
-          await import("https://pyscript.net/releases/2025.11.2/core.js");
-        </script>
-    </head>
-    <body>
-        <mpy-config>
-          packages = ["./reflected_ffi-0.0.1-py3-none-any.whl"]
-        </mpy-config>
-        <py-config>
-          packages = ["./reflected_ffi-0.0.1-py3-none-any.whl"]
-        </py-config>
-        <script type="mpy">
-          import js
-          from datetime import datetime
-          from reflected_ffi.direct import encode, decode
-          print("py", decode(encode(datetime.now())))
-          print("js", decode(js.encode(js.Date.new())))
-          js.console.log(js.decode(encode(datetime.now())))
-        </script>
-    </body>
-</html>
+#           // be sure PyScript core is imported after global utilities are defined
+#           await import("https://pyscript.net/releases/2025.11.2/core.js");
+#         </script>
+#     </head>
+#     <body>
+#         <mpy-config>
+#           packages = ["./reflected_ffi-0.0.1-py3-none-any.whl"]
+#         </mpy-config>
+#         <py-config>
+#           packages = ["./reflected_ffi-0.0.1-py3-none-any.whl"]
+#         </py-config>
+#         <script type="mpy">
+#           import js
+#           from datetime import datetime
+#           from reflected_ffi.direct import encode, decode
+#           print("py", decode(encode(datetime.now())))
+#           print("js", decode(js.encode(js.Date.new())))
+#           js.console.log(js.decode(encode(datetime.now())))
+#         </script>
+#     </body>
+# </html>
 
 
-' > pypi/index.html
+# ' > pypi/index.html
 
-# TODO: this is needed only in coincident
-unzip -o ./pypi/reflected_ffi-0.0.1-py3-none-any.whl -d ./pypi/
-cp -R ./pypi/reflected_ffi ~/git/coincident/src/server/
+# # TODO: this is needed only in coincident
+# unzip -o ./pypi/reflected_ffi-0.0.1-py3-none-any.whl -d ./pypi/
+# cp -R ./pypi/reflected_ffi ~/git/coincident/src/server/
